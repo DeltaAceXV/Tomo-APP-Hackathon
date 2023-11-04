e3026fd61af0013bda0d6b9289302925dc36c0e8
Denise Khew <denisekhew@gmail.com>
2023-11-04 16:36:00 +0800

    Updated main and navigation

--- lib/main.dart
+++ lib/main.dart
@@ -4,21 +4,26 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:tracklocation/View/AuthPage.dart';
+import 'package:tracklocation/View/Buddy/BuddySettings.dart';
+import 'package:tracklocation/View/Buddy/InviteBuddy.dart';
+import 'package:tracklocation/View/Home/AddTasks.dart';
+import 'package:tracklocation/View/Home/CheckInTasks.dart';
+import 'package:tracklocation/View/Notifications/BuddyRequest.dart';
+import 'package:tracklocation/View/Notifications/Notifications.dart';
+import 'package:tracklocation/View/Space/BuddySpace.dart';
+import 'package:tracklocation/navigation.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
-  await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]).then((_) => runApp(const MainApp()));
+  // await SystemChrome.setPreferredOrientations(<DeviceOrientation>[
+  //   DeviceOrientation.portraitUp,
+  //   DeviceOrientation.portraitDown
+  // ]).then((_) => runApp(const MainApp()));
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
@@ -37,10 +42,28 @@ class MainApp extends StatelessWidget {
       title: 'Track Current Location App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.grey),
-        platform: TargetPlatform.android
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        fontFamily: 'Inter',
+        platform: TargetPlatform.android,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+          elevation: 0,
+          titleTextStyle: TextStyle(fontSize: 24, color: Colors.black),
+        )
+
       ),
-      home: const AuthPage(),
+      home: NavigationHomeScreen(),
+      routes: {
+        '/buddyspace': (context) => const BuddySpace(),
+        '/addtasks': (context) => const AddTasks(),
+        '/invitebuddy': (content) => const InviteBuddy(),
+        '/buddysettings': (context) => const BuddySettings(),
+        '/notifications': (context) => const Notifications(),
+        '/buddyrequest': (context) => const BuddyRequest(),
+        '/checkintasks': (context) => const CheckInTasks(),
+        // '/buddyspace': (context) => const BuddySpace(),
+      },
     );
   }
 }
--- lib/navigation.dart
+++ lib/navigation.dart
@@ -2,12 +2,17 @@
 
 import 'package:flutter/material.dart';
 import 'package:tracklocation/Repositories/LoginRepository.dart';
+import 'package:tracklocation/View/Buddy/InviteBuddy.dart';
 import 'package:tracklocation/View/Home/HomePage.dart';
+import 'package:tracklocation/View/Profile/ProfilePage.dart';
 import 'package:tracklocation/View/Profile/UserProfile.dart';
+import 'package:tracklocation/View/Space/BuddyList.dart';
+import 'package:tracklocation/View/Space/BuddySpace.dart';
+import 'package:tracklocation/View/Stats/StatsPage.dart';
 
 import 'DTO/LoginDTO.dart';
 
-class NavigationHomeScreen extends StatefulWidget{
+class NavigationHomeScreen extends StatefulWidget {
   @override
   State<NavigationHomeScreen> createState() => _NavigationHomeScreenState();
 }
@@ -17,148 +22,123 @@ class _NavigationHomeScreenState extends State<NavigationHomeScreen> {
   var selectedIndex = 0;
 
   @override
-   Widget build(BuildContext context) {
-
+  Widget build(BuildContext context) {
     Widget page;
-    switch(selectedIndex){
+
+    // List pages = [
+    //   HomePage(),
+    //   BuddyList(),
+    //   BuddySpace(),
+    //   InviteBuddy()
+    // ];
+
+    switch (selectedIndex) {
       case 0:
         page = HomePage();
         break;
       case 1:
-        page = const Placeholder();
+        page = const BuddyList();
+        break;
+      case 2:
+        page = const StatsPage();
+        break;
+      case 3:
+        page = const ProfilePage();
         break;
       default:
         throw UnimplementedError("No widget for $selectedIndex");
     }
-    return LayoutBuilder(
-      builder: (context, constraints) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    GestureDetector(
-                      onTap: (){
-                        
-                      },
-                      child: const MouseRegion(
-                        child: Icon(Icons.add_rounded),
-                        cursor: SystemMouseCursors.click,
-                      ),
-                    ),
 
-                    SizedBox(width: 100,),
-
-                    GestureDetector(
-                      onTap: () async {
-                        UserDTO? currentUser = (await LoginRepository().getLoginUser()).first;
-
-                        if(currentUser.AuthenticationID == "-"
-                        || currentUser.AuthenticationID == ""){
-                          currentUser = UserDTO();
-
-                          await LoginRepository().addUserDetails(currentUser);
-                        }
-                        
-                        Navigator.push(
-                          context, 
-                          MaterialPageRoute(
-                            builder: (context) {
-                              return UserProfile();
-                            },
-                          )
-                        );
-                      },
-                      child: const MouseRegion(
-                        child: Icon(Icons.person),
-                        cursor: SystemMouseCursors.click,
-                      ),
-                    ),
-                    
-                  ],
-                )
-              ],
-            )
-          ),
-
-          bottomNavigationBar: BottomNavigationBar(
-            
+    return LayoutBuilder(builder: (context, constraints) {
+      return Scaffold(
+        bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
             currentIndex: selectedIndex,
             selectedItemColor: Colors.black,
-
+            backgroundColor: Colors.white,
+            // elevation: 1,
             onTap: (value) {
               setState(() {
                 selectedIndex = value;
               });
             },
-
+            showUnselectedLabels: false,
             items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-                icon: const Icon(Icons.home),
-                label: "Home",
-                activeIcon: const Icon(Icons.home_filled)
-            ),BottomNavigationBarItem(
-                icon: const Icon(Icons.favorite),
-                label: "Favourites",
-                activeIcon: const Icon(Icons.favorite, fill: 1,)
-            ),
-            
-          ]),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.home),
+                  label: "Home",
+                  activeIcon: const Icon(Icons.home_filled)),
+              // BottomNavigationBarItem(
+              //     icon: const Icon(Icons.favorite),
+              //     label: "Favourites",
+              //     activeIcon: const Icon(
+              //       Icons.favorite,
+              //       fill: 1,
+              //     )),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.chat_rounded),
+                  label: "Space",
+                  activeIcon: const Icon(
+                    Icons.chat_rounded,
+                    fill: 1,
+                  )),
 
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.bar_chart_rounded),
+                  label: "Stats",
+                  activeIcon: const Icon(
+                    Icons.bar_chart_rounded,
+                    fill: 1,
+                  )),
+              BottomNavigationBarItem(
+                  icon: const Icon(Icons.person_rounded),
+                  label: "Profile",
+                  activeIcon: const Icon(
+                    Icons.person_rounded,
+                    fill: 1,
+                  )),
+            ]),
+        body: page,
 
-          body: Row(
-            children: [
-              Expanded(child: Container(
-                child: page
-              ))
-            ],
-          )
-          
-          // body: Row(
-          //   children: [
-          //     SafeArea(
-          //       child: NavigationRail(
-          //         extended: constraints.maxWidth >= 600,
-          //         destinations: const [
-          //          NavigationRailDestination(
-          //             icon: const Icon(Icons.home),
-          //             label: const Text('Home', style: TextStyle(inherit: true, color: Colors.black),),
-          //             selectedIcon: const Icon(Icons.home_filled)
-          //           ),
-          //          NavigationRailDestination(
-          //             icon: const Icon(Icons.favorite),
-          //             label: const Text('Favorites'),
-          //             selectedIcon: const Icon(Icons.favorite_rounded)
-          //           ),
-          //         ],
-          //         selectedIndex: selectedIndex,
-          //         onDestinationSelected: (value) {
-          //           setState(() {
-          //             selectedIndex = value;
-          //           });
-          //         },
-                  
-          //       ),
-          //     ),
-          //     Expanded(
-          //       child: Container(
-          //         color: Theme.of(context).colorScheme.primaryContainer,
-          //         child: page,
-          //       ),
-          //     ),
-          //   ],
-          // ),
-        );
-      }
-    );
+        // Row(
+        //   children: [Expanded(child: Container(child: page, color: Colors.green))],
+        // )
+
+        // body: Row(
+        //   children: [
+        //     SafeArea(
+        //       child: NavigationRail(
+        //         extended: constraints.maxWidth >= 600,
+        //         destinations: const [
+        //          NavigationRailDestination(
+        //             icon: const Icon(Icons.home),
+        //             label: const Text('Home', style: TextStyle(inherit: true, color: Colors.black),),
+        //             selectedIcon: const Icon(Icons.home_filled)
+        //           ),
+        //          NavigationRailDestination(
+        //             icon: const Icon(Icons.favorite),
+        //             label: const Text('Favorites'),
+        //             selectedIcon: const Icon(Icons.favorite_rounded)
+        //           ),
+        //         ],
+        //         selectedIndex: selectedIndex,
+        //         onDestinationSelected: (value) {
+        //           setState(() {
+        //             selectedIndex = value;
+        //           });
+        //         },
+
+        //       ),
+        //     ),
+        //     Expanded(
+        //       child: Container(
+        //         color: Theme.of(context).colorScheme.primaryContainer,
+        //         child: page,
+        //       ),
+        //     ),
+        //   ],
+        // ),
+      );
+    });
   }
 }
-
-
-
-
-
-
-
